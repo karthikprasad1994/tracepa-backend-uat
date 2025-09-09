9ea9ad6ae72953993bba8c390f81b848c2cb029d
ShriHarsha <s17177363@gmail.com>
2025-09-09 14:59:22 +0530

    pushed changes

---
+++ Dto/CustomerMaster/OrganizationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TracePca.Dto.CustomerMaster
+{
+    public class OrganizationDto
+    {
+        public int OrgId { get; set; }
+        public string OrgName { get; set; }
+    }
+}
--- Interface/CustomerMasterInterface.cs
+++ Interface/CustomerMasterInterface.cs
@@ -5,6 +5,11 @@ namespace TracePca.Interface
     public interface CustomerMasterInterface
     {
         Task<IEnumerable<CustomerDetailsDto>> GetCustomersWithStatusAsync(int companyId);
+        Task<IEnumerable<ServicesDto>> GetServicesAsync(int companyId);
+        Task<IEnumerable<OrganizationDto>> GetOrganizationsAsync(int companyId);
+        Task<IEnumerable<IndustryTypeDto>> GetIndustryTypesAsync(int companyId);
+        Task<IEnumerable<ManagementTypeDto>> GetManagementTypesAsync(int companyId);
+        Task<string> SaveCustomerMasterAsync(CreateCustomerMasterDto dto);
 
     }
 }
