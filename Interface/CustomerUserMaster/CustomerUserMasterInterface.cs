94a05673571bedcf9afa227ea5b84f02716b4e4e
ShriHarsha <s17177363@gmail.com>
2025-09-12 10:50:49 +0530

    pushed changes

--- Interface/CustomerUserMaster/CustomerUserMasterInterface.cs
+++ Interface/CustomerUserMaster/CustomerUserMasterInterface.cs
@@ -5,5 +5,8 @@ namespace TracePca.Interface.CustomerUserMaster
     public interface CustomerUserMasterInterface
     {
         Task<IEnumerable<CustomerUsersDetailsDto>> GetAllUserDetailsAsync(int companyId);
+        Task<IEnumerable<UserDropdownDto>> LoadExistingUsersAsync(int companyId, string search = "");
+        Task<IEnumerable<CustomerDropdownDto>> LoadActiveCustomersAsync(int companyId);
+        Task<string> InsertCustomerUsersDetailsAsync(CreateCustomerUsersDto dto);
     }
 }
